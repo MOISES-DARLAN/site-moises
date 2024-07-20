a9f34de14a34ee3dc132e4ae63bde569887f7682
MOISES-DARLAN <moisesdarlan440@gmail.com>
2024-07-20 16:15:44 -0300

    modifiquei a api de cursos

--- html/Home/startbootstrap-shop-homepage-gh-pages/cursos.js
+++ html/Home/startbootstrap-shop-homepage-gh-pages/cursos.js
@@ -1,112 +1,123 @@
 let $depoimentos = document.querySelector("#cursos");
+let $carregarMais = document.querySelector("#carregarMais");
+loading();
+acessToServer();
 
 
-// const bd = 
-//     [
-//         {
-//             id: 1,
-//             title: "Curso em vídeo - Python",
-//             img: "https://i.ytimg.com/vi/S9uPNppGsGo/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLD3tGH0TqvdXHXJeCZuWGqjbWULig",
-//             iframe: "https://www.youtube.com/playlist?list=PLvE-ZAFRgX8hnECDn1v9HNTI71veL3oW0"
-//         },
-//         {
-//             id: 2,
-//             title: 'Curso de Javascript Completo',
-//             img: "https://i.ytimg.com/vi/vEwPnjqWQ-g/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDkTOV0l-Eg99hv3hBCP8AXdxC_xA",
-//             iframe: 'https://www.youtube.com/playlist?list=PL2Fdisxwzt_d590u3uad46W-kHA0PTjjw'
-//         },
-//         {
-//             id: 3,
-//             title: 'CURSO DE JAVA - PROGRAMAÇÃO',
-//             img: "https://i.ytimg.com/vi/mRryrODqQcw/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBPVUGTe5qcDczxkifTkwTQCQWDgw",
-//             iframe: 'https://www.youtube.com/playlist?list=PLJ0AcghBBWSi6nK2CUkw9ngvwWB1gE8mL'
-//         },
-//         {
-//             id: 4,
-//             title: "Inglês | Curso Gratuito Completo",
-//             img: 'https://i.ytimg.com/vi/rrUBcqRJFHI/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBnSRVDkkeZFM3hzBUvwFJgWdsxAg',
-//             iframe: "https://www.youtube.com/playlist?list=PLvnDcR9zJ3ToYT9CcWdY3hNuBh4FY99J5"
-//         },
-//         {
-//             id: 5,
-//             title: 'Curso de Linguagem C - 2022 (Gratuito)',
-//             img: 'https://i.ytimg.com/vi/2w8GYzBjNj8/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLADaagxmEXRxqxkITXvB5WeBz6TRA',
-//             iframe: 'https://www.youtube.com/playlist?list=PLpaKFn4Q4GMOBAeqC1S5_Fna_Y5XaOQS2'
-//         },
-//         {
-//             id: 6,
-//             title: 'Curso de Lógica de Programação',
-//             img: 'https://i.ytimg.com/vi/8mei6uVttho/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAeMYmh6Nqfw2DxDJ4Gpud_Alxqeg',
-//             iframe: 'https://www.youtube.com/playlist?list=PLHz_AreHm4dmSj0MHol_aoNYCSGFqvfXV'
-//         },
-//         {
-//             id: 7,
-//             title: 'Curso de Programação C#',
-//             img: 'https://i.ytimg.com/vi/dVzJ3bx68FA/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLCGmGaqXINg2nnIlemsgwmWtOKQKg',
-//             iframe: 'https://www.youtube.com/playlist?list=PLx4x_zx8csUglgKTmgfVFEhWWBQCasNGi'
-//         },
-//         {
-//             id: 8,
-//             title: "CS50 2024 - Curso de Introdução à Programação de Harvard",
-//             img: 'https://i.ytimg.com/vi/mR6jktYAOqs/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLADR7vV2wAjpCu3o9XkLykDKeRvbA'
-//         },
-//         {
-//             id: 9,
-//         title: 'Curso de Java para Iniciantes - Grátis, Completo e com Certificado',
-//            img:  'https://i.ytimg.com/vi/sTX0UEplF54/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAieJQq_q-Nt9HHH1tZ6UgcyCB6dA',
-//            iframe: 'https://www.youtube.com/playlist?list=PLHz_AreHm4dkI2ZdjTwZA4mPMxWTfNSpR'
-//     },
-//     {
-//         id: 10,
-//         title: "Curso de Hardware Grátis",
-//         img: 'https://i.ytimg.com/vi/iT6E92Kt38o/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLApZhCn_R6PFtgTLAhA1PS9fWR65A',
-//         iframe: 'https://www.youtube.com/playlist?list=PLHz_AreHm4dn1JHgN9wpbIUhzZmycYQXW'
-//     }
 
 
-        
-//     ]
-async function acessToServer(){
-    const url = "https://api-cursos-gratis.onrender.com/cursos";
-    const data = await fetch(url, {
-    method: 'GET',
+async function getProducts(category, pulo=0) {
+  const url = `https://demo-project34821.p.rapidapi.com/catalog/category/${category}/products?skip=${pulo}&limit=12`;
+  const options = {
+    method: "GET",
     headers: {
-        'Content-Type': 'application/json'
-    }
-}).then(response => response.json());
+      "x-rapidapi-key": "7bafdafc62msh006083d103e5d7cp107b09jsn07382e7df8ff",
+      "x-rapidapi-host": "demo-project34821.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    const data = result.products;
     return data;
+  } catch (error) {
+    console.error(error);
+  }
 }
-async function loadcursos(){
-    const bd = await acessToServer();
-    bd.forEach(data => carregarDepoimentos(data));
+
+async function getProductsById(data) {
+  let cursos = [];
+  for (const idcursos of data) {
+    const url = `https://demo-project34821.p.rapidapi.com/catalog/product/${idcursos.id}`;
+    const options = {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "7bafdafc62msh006083d103e5d7cp107b09jsn07382e7df8ff",
+        "x-rapidapi-host": "demo-project34821.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      cursos.push(result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return cursos;
 }
 
+async function acessToServer(pulo) {
 
+    try {    
+        const bd = await getProducts("MDCURSOSFREE", pulo);
+        const cursos = await getProductsById(bd);
+        cursos.forEach((data) => { 
+            console.log(data)
+            carregarDepoimentos(data)});
+    }catch (error) {
+        alert('erro ao acessar o servidor:', error)
+    }
 
-loadcursos()
+    removeLoading();
+
+    
+    
+}
+
 function carregarDepoimentos(data) {
-    console.log(data)
-    $depoimentos.innerHTML += `
-              <div class="col mb-5">
-    <div class="card h-100">
+  console.log(data);
+  $depoimentos.innerHTML += `
+    <div class="col mb-5">
+      <div class="card h-100">
         <!-- Product image-->
-        <img class="card-img-top" src="${data.img}" alt="..." />
+        <img class="card-img-top" src="${data.manufacturer}" alt="..." />
         <!-- Product details-->
         <div class="card-body p-4">
-            <div class="text-center">
-                <!-- Product name-->
-                <h5 class="fw-bolder">${data.titulo}</h5>
-
-                Grátis
-            </div>
-
-            
+          <div class="text-center">
+            <!-- Product name-->
+            <h5 class="fw-bolder">${data.name}</h5>
+            Grátis
+          </div>
         </div>
         <!-- Product actions-->
         <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="${data.link}">Ver mais</a></div>
+          <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="${data.description}">Ver mais</a></div>
         </div>
-    </div>
-</div>`;
+      </div>
+    </div>`;
+}
+
+function loading() {
+  for (let i = 0; i < 6; i++) {
+    $depoimentos.innerHTML += `
+    <div class="card col mb-5 mx-3 py-5 loading" aria-hidden="true">
+                        
+                        <div class="card-body">
+                          <h5 class="card-title placeholder-glow">
+                            <span class="placeholder col-6"></span>
+                          </h5>
+                          <p class="card-text placeholder-glow">
+                            <span class="placeholder col-7"></span>
+                            <span class="placeholder col-4"></span>
+                            <span class="placeholder col-4"></span>
+                            <span class="placeholder col-6"></span>
+                            <span class="placeholder col-8"></span>
+                          </p>
+                          <a class="btn btn-primary disabled placeholder col-6" aria-disabled="true"></a>
+                        </div>
+                      </div>`
+
+}
+
+}
+
+function removeLoading() {
+  const cards = document.querySelectorAll(".loading");
+  cards.forEach((card) => {
+    card.classList.add('d-none');
+  })
 }
 
--- html/Home/startbootstrap-shop-homepage-gh-pages/js/cadCurso.js
+++ html/Home/startbootstrap-shop-homepage-gh-pages/js/cadCurso.js
@@ -5,26 +5,42 @@ const link = document.getElementById("link");
 const btnCadastrar = document.querySelector(".btn-primary");
 
 btnCadastrar.addEventListener("click", (event) => {
-    event.preventDefault();
+  event.preventDefault();
+
+  if(!titulo.value || !imagem.value || !link.value) {
+    return alert("Preencha todos os campos");
+  }
+
+  else{
     cadastrar();
-})
+  }
+  
+});
 
 async function cadastrar() {
+  const url = "https://demo-project34821.p.rapidapi.com/catalog/product";
+  const options = {
+    method: "POST",
+    headers: {
+      "x-rapidapi-key": "7bafdafc62msh006083d103e5d7cp107b09jsn07382e7df8ff",
+      "x-rapidapi-host": "demo-project34821.p.rapidapi.com",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: titulo.value,
+      price: 3,
+      manufacturer: imagem.value,
+      category: "MDCURSOSFREE",
+      description: link.value,
+      tags: "CURSOS",
+    }),
+  };
 
-    const dados = {
-        titulo: titulo.value,
-        img: imagem.value,
-        link: link.value
-    }
-    const response = await fetch("https://api-cursos-gratis.onrender.com/cadastrarcursos", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(dados)
-    }).then(()=>{
-        window.location.href = "/html/Home/startbootstrap-shop-homepage-gh-pages/index.html";
-    });
-    const data = await response.json();
-    
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    window.location.href = "index.html";
+  } catch (error) {
+    console.log(error);
+  }
 }
